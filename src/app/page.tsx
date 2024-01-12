151e89936a0dcdc53a931dca2e3948024ceacc65
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2024-01-12 12:11:50 +0900

    とりあえずコミット

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,56 @@
 "use client";
 
-import { Box } from "@yamada-ui/react";
+import {
+  Box,
+  Text,
+  Heading,
+  VStack,
+  HStack,
+  Link,
+  Container,
+  Button,
+  Spacer,
+  Image,
+} from "@yamada-ui/react";
+
 import React, { useState } from "react";
 
 export default function Home() {
   return (
     <Box w="full" h="full" p="md" display="flex" justifyContent="center">
       <Box maxW="4xl">
-        <Box
-          gap="md"
-          h="full"
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          justifyContent="center"
-        >
-          TOP Page
-        </Box>
+        <VStack w="full">
+          <HStack>
+            <Heading>アプリ名</Heading>
+          </HStack>
+          <HStack justifyContent="flex-end">
+            <Link
+              href=""
+            >
+              ランキング
+            </Link>
+            <Link
+              href=""
+            >
+              アチーブメント
+            </Link>
+          </HStack>
+        </VStack>
+        <Container>
+          <HStack>
+            <VStack>
+              <Spacer />
+              <Heading>道徳心を試せ！！</Heading>
+              <Heading>新感覚型対戦ゲーム</Heading>
+              <HStack>
+                {/* リンク系はどうすればいいかわからないのでお願いします！ 一応Buttonにしておきました！ */}
+                <Button>対戦する</Button>
+              </HStack>
+            </VStack>
+            {/* ここに画像が入るかも */}
+          </HStack>
+        </Container>
       </Box>
-    </Box>
+    </Box >
   );
 }
