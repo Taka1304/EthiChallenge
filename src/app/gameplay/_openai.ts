8c4b4ec1682b2846aeff7d8c9fb68fcbb7416f2c
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2024-01-13 18:07:49 +0900

    Fix: 問題生成の返り値をstringで固定

--- src/app/gameplay/_openai.ts
+++ src/app/gameplay/_openai.ts
@@ -10,7 +10,7 @@ const endpoint = process.env.AZURE_OPENAI_ENDPOINT!;
 const azureApiKey = process.env.AZURE_OPENAI_KEY!;
 
 export async function makeQuestion(level: GameLevel) {
-  return new Promise<string | null | undefined>(async (resolve) => {
+  return new Promise<string>(async (resolve, reject) => {
     const client = new OpenAIClient(
       endpoint,
       new AzureKeyCredential(azureApiKey),
@@ -21,8 +21,9 @@ export async function makeQuestion(level: GameLevel) {
     const prompt = makeQ[level] as ChatRequestMessage[];
 
     const deploymentName = "GPT35TURBO16K";
-    const result = await client.getChatCompletions(deploymentName, prompt);
-
-    resolve(result.choices[0].message?.content);
+    const res = await client.getChatCompletions(deploymentName, prompt);
+    const result = res.choices[0].message?.content;
+    if (result) resolve(result);
+    reject("error");
   });
 }
